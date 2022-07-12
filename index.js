e1eb45d3f5b56941bb0a1eb3f5372db75698aefa
bmonish <monishmonib@gmail.com>
2022-07-12 14:17:27 +0530

    CORS origin Support

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const cors = require("cors");
 
 const app = express();
 
 connectDB();
 
 app.use(express.json({ extended: false }));
+app.use(cors());
 
 app.get("/", (req, res) => res.send("Appointment API running"));
 
